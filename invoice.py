99b8217d5fab1b6fef7d5b34b54328fe2e770fff
?ngel ?lvarez <angel@nan-tic.com>
2019-04-28 19:08:18 +0200

    adapt to ir.message

--- invoice.py
+++ invoice.py
@@ -1,21 +1,15 @@
 # This file is part account_invoice_validate_identifier module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import PoolMeta, Pool
+from trytond.i18n import gettext
+from trytond.exceptions import UserWarning
 
 __all__ = ['Invoice']
 
 
 class Invoice(metaclass=PoolMeta):
     __name__ = 'account.invoice'
-
-    @classmethod
-    def __setup__(cls):
-        super(Invoice, cls).__setup__()
-        cls._error_messages.update({
-                'missing_party_identifier': ('There is no identifer on party '
-                    '"%(party)s" and invoice "%(invoice)s".'),
-                })
 
     @classmethod
     def validate(cls, invoices):
@@ -26,10 +20,13 @@ class Invoice(metaclass=PoolMeta):
             invoice.check_party_identifier()
 
     def check_party_identifier(self):
+        Warning = Pool().get('res.user.warning')
         if not self.party.identifiers:
             key = 'invoice.party.identifier%s_%s' % (
                 self.id, self.party.id)
-            self.raise_user_warning(key, 'missing_party_identifier', {
-                    'party': self.party.rec_name,
-                    'invoice': self.rec_name,
-                    })
+
+            if Warning.check(key):
+                raise UserWarning(key, gettext(
+                    'account_invoice_validate_identifier.missing_party_identifier',
+                    party=self.party.rec_name,
+                    invoice=self.rec_name))
